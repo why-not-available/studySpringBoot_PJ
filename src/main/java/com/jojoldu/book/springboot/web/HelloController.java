f59bfbbc0c4d54ceff2dbba4b2efd14bdfb966d5
why-not-available <aever144@naver.com>
2024-02-06 21:54:22 +0900

    2장 막힘

---
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어줌
+@RestController
+public class HelloController {
+//  HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어줌
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMVC mvc;
+
+    @Test
+    public void hello_returns() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
