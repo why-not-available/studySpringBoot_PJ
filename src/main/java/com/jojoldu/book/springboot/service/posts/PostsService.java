9ccd33a7a16cb2d220e2c7d1abc9c3988e59e256
why-not-available <aever144@naver.com>
2024-02-26 17:53:50 +0900

    3장 4.등록/수정/조회 API 만들기 : PostApiControllerTest >> 해결인듯 : Cannot invoke ~ because "this.restTemplate" is null

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -14,6 +14,7 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
+
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 }
